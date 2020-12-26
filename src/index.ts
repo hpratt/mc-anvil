58be85e3e92792013cacab51a6b9d3cd4ae17e2b
hpratt <hpratt111@live.com>
2020-12-26 15:23:40 -0500

    export TagType as value

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { BinaryParser } from './util';
 export { AnvilParser } from './anvil';
 export type { ChunkDataDescriptor, CompressionType, LocationEntry } from './anvil';
-export { NBTParser } from './nbt';
-export type { TagType, TagData, TagPayload } from './nbt';
+export { NBTParser, TagData } from './nbt';
+export type { TagType, TagPayload } from './nbt';
