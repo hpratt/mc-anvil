37a700d68575eb242b1f4896b2a77f598083587a
hpratt <hpratt111@live.com>
2020-12-26 15:32:52 -0500

    fix typo

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { BinaryParser } from './util';
 export { AnvilParser } from './anvil';
 export type { ChunkDataDescriptor, CompressionType, LocationEntry } from './anvil';
-export { NBTParser, TagData } from './nbt';
-export type { TagType, TagPayload } from './nbt';
+export { NBTParser, TagType } from './nbt';
+export type { TagData, TagPayload } from './nbt';
