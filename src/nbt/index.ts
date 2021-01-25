7cf5f5104d9629e54c9a23a6f71afbb2f2105cff
hpratt <hpratt111@live.com>
2021-01-24 20:21:05 -0500

    export ListPayload

--- src/nbt/index.ts
+++ src/nbt/index.ts
@@ -1,5 +1,5 @@
 export { NBTParser, findChildTagAtPath, findChildTag, findCompoundListChildren } from './nbt';
 export { TagType } from './types';
-export type { TagData, TagPayload } from './types';
+export type { TagData, TagPayload, ListPayload } from './types';
 export { nbtTagReducer, NBTActions } from './reducer';
 export type { NBTAction } from './reducer';
